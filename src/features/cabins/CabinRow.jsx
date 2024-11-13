5b51de58d7693686c0b795c20bd6569e4241672e
Mice Karajov <38554954+micekarajov@users.noreply.github.com>
2024-11-13 16:28:07 +0100

    Implement Filtering Cabin Data(on client side) Feature

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -9,18 +9,20 @@ import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
 
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
 
 const Img = styled.img`
     display: block;
@@ -75,7 +77,7 @@ function CabinRow({ cabin }) {
     }
 
     return (
-        <TableRow role="row">
+        <Table.Row>
             <Img src={image} />
 
             <Cabin>{name}</Cabin>
@@ -91,35 +93,46 @@ function CabinRow({ cabin }) {
             )}
 
             <div>
-                <button onClick={handleDuplicate} disabled={isCreating}>
-                    <HiSquare2Stack />
-                </button>
-
                 <Modal>
-                    <Modal.Open opens="edit">
-                        <button>
-                            <HiPencil />
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window name="edit">
-                        <CreateCabinForm cabinToEdit={cabin} />
-                    </Modal.Window>
+                    <Menus.Menu>
+                        <Menus.Toggle id={cabinId} />
 
-                    <Modal.Open opens="delete">
-                        <button>
-                            <HiTrash />
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window name="delete">
-                        <ConfirmDelete
-                            resourceName="cabins"
-                            disabled={isDeliting}
-                            onConfirm={() => deleteCabin(cabinId)}
-                        />
-                    </Modal.Window>
+                        <Menus.List id={cabinId}>
+                            <Menus.Button
+                                icon={<HiSquare2Stack />}
+                                onClick={handleDuplicate}
+                            >
+                                Duplicate
+                            </Menus.Button>
+
+                            <Modal.Open opens="edit">
+                                <Menus.Button icon={<HiPencil />}>
+                                    Edit
+                                </Menus.Button>
+                            </Modal.Open>
+
+                            <Modal.Open opens="delete">
+                                <Menus.Button icon={<HiTrash />}>
+                                    Delete
+                                </Menus.Button>
+                            </Modal.Open>
+                        </Menus.List>
+
+                        <Modal.Window name="edit">
+                            <CreateCabinForm cabinToEdit={cabin} />
+                        </Modal.Window>
+
+                        <Modal.Window name="delete">
+                            <ConfirmDelete
+                                resourceName="cabins"
+                                disabled={isDeliting}
+                                onConfirm={() => deleteCabin(cabinId)}
+                            />
+                        </Modal.Window>
+                    </Menus.Menu>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
